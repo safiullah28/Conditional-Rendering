7363e9ac662a28947075f6f565cee118321079e5
safiullah28 <safiullah424@gmail.com>
2024-03-13 00:12:38 +0500

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import Agevalidation from './Agevalidation';
 import './App.css';
+import Name from './Name';
 
 function App() {
+  //Destructuring
+  // const arr=[1,2,3,4];
+  // const [one,two,four]=arr;
+  // console.log(one,"One");
+  // console.log(two,"two");
+  // console.log(four,"Four");
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  {/*<Name fname="Safi" lname="ullah"/>*/}
+  <Agevalidation/>
     </div>
   );
 }
